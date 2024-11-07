896775922171ac2714e2161a42ac56b3eb44ef14
Richie-Z <zakariarichie@yahoo.com>
2024-11-07 16:38:34 +0700

    feat: toggleable layout

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -44,7 +44,7 @@ class AdminPanelProvider extends PanelProvider
                     ->displayToggleAction()
                     ->toggleActionHook('tables::toolbar.search.after')
                     ->listLayoutButtonIcon('heroicon-o-list-bullet')
-                    ->gridLayoutButtonIcon('heroicon-o-grid-2x2'),
+                    ->gridLayoutButtonIcon('heroicon-o-squares-2x2'),
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
