c449dd1a49817cf4652b3ad3a4edb547e839d2b8
C-BESSY <charistyfirst@gmail.com>
2024-11-08 08:52:51 +0800

    feat: hasToggleTable

--- app/Filament/Resources/ReimbursementFormResource/Pages/ListReimbursementForms.php
+++ app/Filament/Resources/ReimbursementFormResource/Pages/ListReimbursementForms.php
@@ -5,9 +5,12 @@ namespace App\Filament\Resources\ReimbursementFormResource\Pages;
 use App\Filament\Resources\ReimbursementFormResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Hydrat\TableLayoutToggle\Concerns\HasToggleableTable;
 
 class ListReimbursementForms extends ListRecords
 {
+    use HasToggleableTable;
+
     protected static string $resource = ReimbursementFormResource::class;
 
     protected function getHeaderActions(): array
