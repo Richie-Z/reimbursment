dd467c76f6598bea6d080ce59e0e2a2e35b8a1e4
C-BESSY <charistyfirst@gmail.com>
2024-11-08 08:55:07 +0800

    enhance: tabel for PDF

--- app/Filament/Resources/ReimbursementFormResource.php
+++ app/Filament/Resources/ReimbursementFormResource.php
@@ -9,11 +9,10 @@ use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Forms\Components\Split;
 use Filament\Tables\Table;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\Group;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Toggle;
@@ -39,124 +38,91 @@ class ReimbursementFormResource extends Resource
     {
         return $form
             ->schema([
-                Section::make('')
-                    ->schema([
-                        Select::make('user_id')
-                            ->label('Pake Duid Siapaa?')
-                            ->options(User::all()->pluck('name', 'id'))
-                            ->required(),
-                        TextInput::make('price')
-                            ->label('Berapa Nominalnya?')
-                            ->numeric()
-                            ->reactive()
-                            ->prefix('Rp')
-                            ->extraInputAttributes(['inputmode' => 'numeric'])
-                            ->required(),
-                        DatePicker::make('date')
-                            ->label('Kapan?')
-                            ->required(),
-                        TextInput::make('title')
-                            ->label('Keperluan apa?')
-                            ->columnSpanFull()
-                            ->required(),
-                    ])->columnSpan(2)->columns(3),
-                Group::make()
-                    ->schema([
-                        Section::make('Perlu Before-After??')
-                            ->schema([
-                                Toggle::make('documentation_needed')
-                                    ->label('Yoi')
-                                    ->live()
-                                    ->columnSpan(1)
-                                    ->default(false),
-                                Grid::make(2)
-                                    ->schema([
-                                        FileUpload::make('before')
-                                            ->label('Dokumentasi Before')
-                                            ->hidden(fn(\Filament\Forms\Get $get) => !$get('documentation_needed'))
-                                            ->maxSize(2048)
-                                            ->required(),
-                                        FileUpload::make('after')
-                                            ->label('Dokumentasi After')
-                                            ->hidden(fn(\Filament\Forms\Get $get) => !$get('documentation_needed'))
-                                            ->maxSize(2048)
-                                            ->required(),
-                                    ]),
-                                FileUpload::make('documentation')
-                                    ->label('Dokumentasi')
-                                    ->hidden(fn(\Filament\Forms\Get $get) => $get('documentation_needed'))
-                                    ->maxSize(2048)
-                                    ->required(),
-                            ])
-                    ])
+                Section::make()->schema([
+                    Toggle::make('documentation_needed')
+                        ->label('Dokumentasi perlu After & Before')
+                        ->live()
+                        ->columnSpan(1)
+                        ->hintIcon('heroicon-o-information-circle')
+                        ->hintIconTooltip('Jika iya, maka wajib upload dokumentasi')
+                        ->default(false),
+                ]),
+                Split::make([
+                    Section::make('')
+                        ->schema([
+                            Select::make('user_id')
+                                ->label('Pake Duid Siapaa?')
+                                ->options(User::all()->pluck('name', 'id'))
+                                ->required(),
+                            DatePicker::make('date')
+                                ->label('Kapan?')
+                                ->required(),
+                            TextInput::make('price')
+                                ->label('Berapa Nominalnya?')
+                                ->numeric()
+                                ->reactive()
+                                ->prefix('Rp')
+                                ->extraInputAttributes(['inputmode' => 'numeric'])
+                                ->required()
+                                ->columnSpanFull(),
+                            TextInput::make('title')
+                                ->label('Keperluan apa?')
+                                ->columnSpanFull()
+                                ->required(),
+                        ])->columns(2)->columnSpanFull(),
+
+                    Section::make('Upload Dokumenasi ')
+                        ->description('Upload Dokumentasi anda')
+                        ->schema([
+                            FileUpload::make('before')
+                                ->label('Dokumentasi Before')
+                                ->hidden(fn(\Filament\Forms\Get $get) => !$get('documentation_needed'))
+                                ->maxSize(2048)
+                                ->image()
+                                ->disk('public')
+                                ->directory('doc_before')
+                                ->required(),
+                            FileUpload::make('after')
+                                ->label('Dokumentasi After')
+                                ->hidden(fn(\Filament\Forms\Get $get) => !$get('documentation_needed'))
+                                ->image()
+                                ->disk('public')
+                                ->directory('doc_after')
+                                ->maxSize(2048)
+                                ->required(),
+                            FileUpload::make('documentation')
+                                ->label('Dokumentasi')
+                                ->hidden(fn(\Filament\Forms\Get $get) => $get('documentation_needed'))
+                                ->image()
+                                ->disk('public')
+                                ->directory('doc')
+                                ->maxSize(2048)
+                                ->required(),
+                        ])
+                ])->columnSpanFull(),
+
             ])->columns(['default' => 3, 'sm' => 3, 'md' => 3, 'lg' => 3]);
     }
 
     public static function table(Table $table): Table
     {
+        $livewire = $table->getLivewire();
+
         return $table
-            ->columns([
-                Tables\Columns\Layout\Grid::make()
-                    ->columns(1)
-                    ->schema([
-                        Tables\Columns\Layout\Split::make([
-                            Tables\Columns\Layout\Grid::make()
-                                ->columns(1)
-                                ->schema([
-                                    ImageColumn::make('documentation')
-                                        ->height(200)
-                                        ->width(150)
-                                        ->url(fn($record) => asset('storage/' . $record->documentation))
-                                        ->extraImgAttributes([
-                                            'class' => 'rounded-full',
-                                        ]),
-                                    ImageColumn::make('before')
-                                        ->height(100)
-                                        ->width(100)
-                                        ->url(fn($record) => asset('storage/' . $record->before))
-                                        ->extraImgAttributes([
-                                            'class' => 'rounded-full',
-                                        ]),
-                                    ImageColumn::make('after')
-                                        ->height(100)
-                                        ->width(100)
-                                        ->url(fn($record) => asset('storage/' . $record->after))
-                                        ->extraImgAttributes([
-                                            'class' => 'rounded-full',
-                                        ])
-                                ])->grow(false),
-                            Tables\Columns\Layout\Stack::make([
-                                Tables\Columns\TextColumn::make('user.name')
-                                    ->weight(FontWeight::Medium),
-                                Tables\Columns\TextColumn::make('title')
-                                    ->wrap(),
-                                Tables\Columns\TextColumn::make('price')
-                                    ->numeric(decimalPlaces: 0)
-                                    ->prefix('Rp '),
-                                Tables\Columns\TextColumn::make('date'),
-                                ToggleColumn::make('is_paid')
-                                    ->label('Paid')
-                                    ->onIcon('heroicon-o-check')
-                                    ->offIcon('heroicon-o-x-mark')
-                                    ->action(function ($record, $state) {
-                                        $record->update(['is_paid' => $state]);
-                                    })
-                                    ->sortable()
-                                    ->toggleable(),
-                            ])->extraAttributes(['class' => 'space-y-2'])
-                                ->grow(),
-                        ])
-                    ])
-            ])
+            ->columns(
+                $livewire->isGridLayout() ?
+                    static::getGridTableLayout() :
+                    static::getListTableLayout()
+            )
             ->contentGrid([
-                'md' => 2,
-                'xl' => 3,
+                'md' => 1,
+                'xl' => 2,
             ])
             ->recordUrl(false)
             ->paginationPageOptions([10, 20, 30, 40, 50])
             ->filters([
                 TernaryFilter::make('is_paid')->label('Pembayaran')->indicator('Pembayaran'),
-                SelectFilter::make('user_id')->label('User')->options(user::all()->pluck('name', 'id'))
+                SelectFilter::make('user_id')->label('User')->options(User::all()->pluck('name', 'id'))
                     ->indicator('User'),
                 // Filter::make('date')->label('Kapan?')->form([
                 //     DatePicker::make('date')->label('Kapan?'),
@@ -174,10 +140,12 @@ class ReimbursementFormResource extends Resource
                         ->icon('heroicon-m-arrow-down-tray')
                         ->openUrlInNewTab()
                         ->deselectRecordsAfterCompletion()
-                        ->action(function (Collection $records) {
-                            return response()->streamDownload(function () use ($records) {
+                        ->action(function (Collection $records, $livewire) {
+                            $hiddenCols = collect($livewire->toggledTableColumns)
+                                ->filter(fn($val) => is_array($val) ? collect($val)->every(fn($arrVal) => !$arrVal) : !$val)->keys()->toArray();
+                            return response()->streamDownload(function () use ($records, $hiddenCols) {
                                 echo Pdf::loadHTML(
-                                    Blade::render('pdf', ['records' => $records])
+                                    Blade::render('pdf', ['records' => $records, 'hiddenCols' => $hiddenCols])
                                 )->stream();
                             }, 'Reimburse.pdf');
                         }),
@@ -185,6 +153,82 @@ class ReimbursementFormResource extends Resource
             ]);
     }
 
+    public static function getListTableLayout(): array
+    {
+        return [...static::getImageCols(), ...static::getTextCols()];
+    }
+
+    public static function getGridTableLayout(): array
+    {
+        return [
+            Tables\Columns\Layout\Grid::make()
+                ->columns(1)
+                ->schema([
+                    Tables\Columns\Layout\Split::make([
+                        Tables\Columns\Layout\Grid::make()
+                            ->columns(1)
+                            ->schema([
+                                ...static::getImageCols(),
+                            ])->grow(false),
+                        Tables\Columns\Layout\Stack::make([
+                            ...static::getTextCols(),
+                        ])->extraAttributes(['class' => 'space-y-2'])
+                            ->grow(),
+                    ])
+                ])
+        ];
+    }
+
+    public static function getImageCols(): array
+    {
+        return [
+            ImageColumn::make('documentation')
+                ->height(200)
+                ->width(150)
+                ->disk('public')
+                ->extraImgAttributes([
+                    'class' => 'rounded-sm',
+                ]),
+            ImageColumn::make('before')
+                ->height(100)
+                ->width(100)
+                ->disk('public')
+                ->extraImgAttributes([
+                    'class' => 'rounded-sm',
+                ]),
+            ImageColumn::make('after')
+                ->height(100)
+                ->width(100)
+                ->disk('public')
+                ->extraImgAttributes([
+                    'class' => 'rounded-sm',
+                ]),
+        ];
+    }
+
+    public static function getTextCols(): array
+    {
+        return [
+            Tables\Columns\TextColumn::make('user.name')
+                ->weight(FontWeight::Medium),
+            Tables\Columns\TextColumn::make('title')
+                ->label('Keperluan')
+                ->wrap(),
+            Tables\Columns\TextColumn::make('price')
+                ->numeric(decimalPlaces: 0)
+                ->prefix('Rp '),
+            Tables\Columns\TextColumn::make('date'),
+            ToggleColumn::make('is_paid')
+                ->label('Paid')
+                ->onIcon('heroicon-o-check')
+                ->offIcon('heroicon-o-x-mark')
+                ->action(function ($record, $state) {
+                    $record->update(['is_paid' => $state]);
+                })
+                ->sortable()
+                ->toggleable(),
+        ];
+    }
     public static function getRelations(): array
     {
         return [
