b09d6e1c6477a6752b0f9cd59b7177bf7b42d381
C-BESSY <charistyfirst@gmail.com>
2024-11-11 12:32:47 +0800

    fix & feat: display inline block; add logo

--- resources/views/pdfPerSheet.blade.php
+++ resources/views/pdfPerSheet.blade.php
@@ -1,6 +1,7 @@
 @foreach ($records as $record)
     <div
         style="text-align: center; font-size: 16px; font-weight: bold; padding-bottom: 10px; border-bottom: 1px solid #ddd; margin-top: 20px;">
+        <img src="{{ public_path('images/Logo.png') }}" style="max-width:100px">
         <h2 style="margin: 0; padding: 0;">REIMBURSEMENT REPORT</h2>
         <p style="font-size: 14px; color: #169; margin: 0; padding: 0;">PLN ICON PLUS UNIT LAYANAN NTT</p>
         <p style="font-size: 12px; color: #555; margin: 0; padding: 0;">Generated on:
@@ -8,10 +9,9 @@
     </div>
 
     <!-- Layout for Side-by-Side Content -->
-    <div>ID : {{ $record->id }}</div>
-    <div style="display: flex; flex-direction: Column;">
-        <!-- Left Column: Text Details Section -->
-        <div style="width: 20%; height: 20%; padding: 10px; border: 1px solid #000; vertical-align: top;">
+    <div style="margin:10px;">ID : {{ $record->id }}</div>
+    <div style="width: 100%; display: inline-block;">
+        <div style="width: 30%; display: inline-block; vertical-align: top; padding: 10px; border: 1px solid #000;">
             @if (!in_array('user', $hiddenCols))
                 <div style="margin-bottom: 10px;">
                     <strong style="font-size: 14px; color: #666;">Name:</strong>
@@ -21,8 +21,9 @@
             @if (!in_array('date', $hiddenCols))
                 <div style="margin-bottom: 10px;">
                     <strong style="font-size: 14px; color: #666;">Date:</strong>
-                    <span
-                        style="font-size: 14px; color: #000">{{ \Carbon\Carbon::parse($record->date)->format('d-m-Y') }}</span>
+                    <span style="font-size: 14px; color: #000;">
+                        {{ \Carbon\Carbon::parse($record->date)->format('d-m-Y') }}
+                    </span>
                 </div>
             @endif
             @if (!in_array('title', $hiddenCols))
@@ -34,7 +35,7 @@
             @if (!in_array('price', $hiddenCols))
                 <div style="margin-bottom: 10px;">
                     <strong style="font-size: 14px; color: #666;">Price:</strong>
-                    <span style="font-size: 14px; color: #000">Rp
+                    <span style="font-size: 14px; color: #000;">Rp
                         {{ number_format($record->price, 0, ',', '.') }}</span>
                 </div>
             @endif
@@ -49,12 +50,13 @@
         </div>
 
         <!-- Right : Image Section -->
-        <div style="width: 50%; padding: 10px; text-align: center; vertical-align: top;">
+        <div
+            style="width: 60%; display: inline-block; padding: 0 0 10px 10px; text-align: center; vertical-align: top;">
             @if ($record->before && $record->after)
                 @if (!in_array('before', $hiddenCols))
                     <div style="margin-bottom: 10px;">
                         <img src="{{ storage_path('app/public/' . $record->before) }}" alt="Before"
-                            style="max-width: 100%; height: auto; border-radius: 8px;">
+                            style="max-width: 100%; height: auto; border-radius: 1px;">
                         <p style="font-size: 12px; color: #555;">Before</p>
                     </div>
                 @endif
