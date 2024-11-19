85403626cc3ab5ee7bc5e696640deaa8a5d929c3
C-BESSY <charistyfirst@gmail.com>
2024-11-19 15:29:41 +0800

    feat: direct after create and edit

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -3,10 +3,13 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Traits\Redirect;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateUser extends CreateRecord
 {
+    use Redirect;
+
     protected static string $resource = UserResource::class;
 }
